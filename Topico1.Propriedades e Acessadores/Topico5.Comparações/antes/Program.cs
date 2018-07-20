c6abeb3af178f6bbd71294deec02f3bdfcae2650
marcelooliveira <mclricardo@gmail.com>
2018-07-20 19:17:37 -0300

    Aula5 - antes

---
+++ Topico1.Propriedades e Acessadores/Topico5.Comparações/antes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Topico5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno
+            {
+                Nome = "José da Silva",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            Aluno aluno2 = new Aluno
+            {
+                Nome = "José da Silva",
+                DataNascimento = new DateTime(1995, 1, 1)
+            };
+
+            Aluno aluno3 = new Aluno
+            {
+                Nome = "josé da silva",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Data nascimento: {DataNascimento:dd/MM/yyyy}";
+        }
+    }
+}
